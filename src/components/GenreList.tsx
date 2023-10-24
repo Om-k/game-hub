ffdb12c6169d37b6cdf83d66c2ea5183356e5167
omkpo <om.k.poojary@gmail.com>
2023-10-24 11:16:10 +0530

    Highlight the selected object and make a new set for all games

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -8,14 +8,19 @@ import {
 } from "@chakra-ui/react";
 import useGenres, { Genre } from "../hooks/useGenres";
 import getCroppedImageUrl from "../services/image-url";
+import allGames from "../assets/All-Games.jpeg";
 
 interface Props {
-  onSelectedGenre: (genre: Genre) => void;
+  onSelectedGenre: (genre: Genre | null) => void;
+  selectedGenre: Genre | null;
 }
 
-const GenreList = ({ onSelectedGenre }: Props) => {
+const GenreList = ({ onSelectedGenre, selectedGenre }: Props) => {
   const { data, isLoading, error } = useGenres();
   if (error) return null;
+  {
+    /* Mycode start */
+  }
   const tempGenre = [
     1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20,
   ];
@@ -38,6 +43,22 @@ const GenreList = ({ onSelectedGenre }: Props) => {
 
   return (
     <List>
+      <ListItem key={0} paddingY="5px">
+        <HStack>
+          <Image src={allGames} boxSize="32px" borderRadius={8} />
+
+          <Button
+            fontSize="large"
+            variant="link"
+            onClick={() => onSelectedGenre(null)}
+            fontWeight={selectedGenre ? "normal" : "bold"}
+          >
+            All
+          </Button>
+        </HStack>
+      </ListItem>
+      {/* Mycode end */}
+
       {data.map((genre) => (
         <ListItem key={genre.id} paddingY="5px">
           <HStack>
@@ -50,6 +71,7 @@ const GenreList = ({ onSelectedGenre }: Props) => {
               fontSize="large"
               variant="link"
               onClick={() => onSelectedGenre(genre)}
+              fontWeight={genre.id === selectedGenre?.id ? "bold" : "normal"}
             >
               {genre.name}
             </Button>
