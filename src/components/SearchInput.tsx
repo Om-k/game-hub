f76ce5ba16e6e68b59362cd76551f4385f8ffea4
omkpo <om.k.poojary@gmail.com>
2023-10-24 17:15:14 +0530

    Implemented searching

--- src/components/SearchInput.tsx
+++ src/components/SearchInput.tsx
@@ -1,12 +1,46 @@
-import { Input, InputGroup, InputLeftElement } from "@chakra-ui/react";
+import {
+  Button,
+  CloseButton,
+  Input,
+  InputGroup,
+  InputLeftElement,
+  InputRightElement,
+} from "@chakra-ui/react";
+import { useRef, useState } from "react";
 import { BsSearch } from "react-icons/bs";
 
-const SearchInput = () => {
+interface Props {
+  onSearch: (searchText: string) => void;
+}
+
+const SearchInput = ({ onSearch }: Props) => {
+  const ref = useRef<HTMLInputElement>(null);
+  const [inputValue, setInputValue] = useState<string>("");
+
   return (
-    <InputGroup>
-      <InputLeftElement children={<BsSearch />} />
-      <Input borderRadius={20} placeholder="Search" variant="filled" />;
-    </InputGroup>
+    <form
+      onSubmit={(event) => {
+        event.preventDefault();
+        if (ref.current) onSearch(ref.current.value);
+      }}
+    >
+      <InputGroup>
+        <InputLeftElement children={<BsSearch />} />
+        <InputRightElement
+          children={<CloseButton />}
+          onClick={() => setInputValue("")}
+        />
+        <Input
+          ref={ref}
+          borderRadius={20}
+          placeholder="Search games"
+          variant="filled"
+          value={inputValue}
+          onChange={(e) => setInputValue(e.target.value)}
+        />
+        ;
+      </InputGroup>
+    </form>
   );
 };
 
