a24d0631690061189432e46458124aac0015eaae
omkpo <om.k.poojary@gmail.com>
2023-10-24 16:07:17 +0530

    Sort the games

--- src/hooks/useGames.ts
+++ src/hooks/useGames.ts
@@ -17,13 +17,15 @@ export interface Game {
     background_image:string;  
     parent_platforms:{platform:Platform}[];
     metacritic:number;
+
   }
 
 const useGames = (gameQuery:GameQuery) => 
 useData<Game>("/games",{
   params:{
     genres:gameQuery.genre?.id,
-    platforms:gameQuery.platform?.id
+    platforms:gameQuery.platform?.id,
+    ordering:gameQuery.sortOrder
   }},[gameQuery]);
 
 export default useGames;
